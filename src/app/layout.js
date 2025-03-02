5cab2bc2a2a3f851959e451bff27a1bf27b0ef09
NGUYEN Dinh Duc Nha <ndducnha@gmail.com>
2025-03-03 06:14:09 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -95,10 +95,10 @@ export default function Layout({ children }) {
           {/* Mobile Menu */}
           {menuOpen && (
             <ul className="lg:hidden mt-4 p-4 space-y-3 rounded-lg">
-              <li><Link href="/" className="block text-[#000022]" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+              <li><Link href="/" className="block text-[#000022]" onClick={() => setMenuOpen(false)}>Home</Link></li>
               <li><Link href="/people" className="block text-[#000022]" onClick={() => setMenuOpen(false)}>People</Link></li>
               <li><Link href="/publication" className="block text-[#000022]" onClick={() => setMenuOpen(false)}>Publications</Link></li>
-              <li><Link href="/join-us" className="block text-[#000022]" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+              <li><Link href="/join-us" className="block text-[#000022]" onClick={() => setMenuOpen(false)}>Join us</Link></li>
               <li><Link href="/contact" className="block text-[#000022]" onClick={() => setMenuOpen(false)}>Contact</Link></li>
             </ul>
           )}
@@ -112,7 +112,7 @@ export default function Layout({ children }) {
           <div className="container mx-auto flex flex-col md:flex-row justify-between items-start">
             <div className="md:w-1/4">
               <img src="/footerlogo.png" alt="Vcyber Logo" className="w-32 mb-4" />
-              <p className="text-sm">Copyright © {new Date().getFullYear()} VinUni.</p>
+              <p className="text-sm">Copyright © {new Date().getFullYear()} vcyber.</p>
               <p className="text-sm">All Rights Reserved.</p>
               <a href="/privacy-policy" className="text-sm underline">Privacy Policy</a>
             </div>
@@ -123,6 +123,7 @@ export default function Layout({ children }) {
               </ul>
               <ul className="space-y-2">
                 <li><Link href="/publication" className="hover:underline">Publications</Link></li>
+                <li><Link href="/join-us" className="hover:underline">Join us</Link></li>
                 <li><Link href="/contact" className="hover:underline">Contact</Link></li>
               </ul>
             </div>
