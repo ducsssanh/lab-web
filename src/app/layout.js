f87eda9a69267added086c1408358b8aabf993ea
NGUYEN Dinh Duc Nha <ndducnha@gmail.com>
2025-03-06 10:56:36 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -139,15 +139,6 @@ export default function Layout({ children }) {
           >
             ⬆️
           </button>
-         <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-9FL93G3YHV"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-9FL93G3YHV');
-</script>
         </footer>
       </body>
     </html>
